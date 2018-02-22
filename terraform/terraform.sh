345be9ddf8e2991327fd228713a494239c00deee
itsdalmo <kristian@doingit.no>
2018-02-22 21:20:04 +0100

    Bump tflint version.

--- terraform/terraform.sh
+++ terraform/terraform.sh
@@ -26,7 +26,7 @@ setup() {
 }
 
 install_tflint() {
-    curl -s -L -o /tmp/tflint.zip https://github.com/wata727/tflint/releases/download/v0.5.3/tflint_linux_amd64.zip
+    curl -s -L -o /tmp/tflint.zip https://github.com/wata727/tflint/releases/download/v0.5.4/tflint_linux_amd64.zip
     unzip -o -q /tmp/tflint.zip -d $DIR/source/cache
     ln -s $DIR/source/cache/tflint /usr/local/bin/tflint
     print success "Download and install tflint"
